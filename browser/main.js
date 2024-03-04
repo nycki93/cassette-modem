e09400e5fc54bef0d22a18f28d7b74380c5b2479
Nicholas Lamicela <nupanick@gmail.com>
2024-03-04 10:10:04 -0800

    make button reusable

--- browser/main.js
+++ browser/main.js
@@ -1,4 +1,5 @@
 const A_TUNING_HZ = 440;
+const MIDDLE_C = -9;
 
 const audio = new AudioContext();
 
@@ -6,17 +7,16 @@ const gainNode = audio.createGain();
 gainNode.gain.value = 1;
 gainNode.connect(audio.destination);
 
-function note_hz(n) {
+function note(n) {
     return A_TUNING_HZ * Math.pow(2, n / 12);
 }
 
-const middleC = note_hz(-9);
-
-const sineNode = audio.createOscillator();
-sineNode.frequency.value = middleC;
-sineNode.connect(gainNode);
-
 document.getElementById('play').onclick = () => {
+    const sineNode = audio.createOscillator();
+    sineNode.type = 'sine';
+    sineNode.frequency.value = note(MIDDLE_C);
+    sineNode.connect(gainNode);
     sineNode.start();
-    sineNode.stop(audio.currentTime + 2);
+    sineNode.stop(audio.currentTime + 0.500);
+    setTimeout(() => sineNode.disconnect(), 500);
 }
