ec807f6b2152ed31a9ceb1cc9c62d9f001fb5752
Nicholas Lamicela <nupanick@gmail.com>
2024-07-27 14:03:08 -0700

    get sample rate

--- index.js
+++ index.js
@@ -3,70 +3,10 @@ function main() {
     const mainDiv = document.getElementById('main');
     mainDiv.innerHTML += '<button id="start">start</button>'
     const startButton = document.getElementById('start');
-    startButton.onclick = start2;
+    startButton.onclick = start;
 }
-
-/**
- * @param {MediaStream} stream 
- * @returns {Generator<number>}
- */
-async function* getSamples(stream) {
-    const recorder = new MediaRecorder(stream, { mimeType: 'audio/webm;codecs='})
-}
-
-// Seems to fail after the first chunk; the remaining chunks don't have headers:
-// https://stackoverflow.com/questions/64744309/how-can-i-get-a-continuous-stream-of-samples-from-the-javascript-audioapi
 
 async function start() {
-    const mic = await navigator.mediaDevices.getUserMedia({ audio: true });
-    const recorder = new MediaRecorder(mic, { mimeType: 'audio/webm;codecs='});
-    const audioContext = new AudioContext();
-    const reader = new FileReader();
-
-    let header;
-
-    /**
-     * @returns {Promise<AudioBuffer>}
-     */
-    function decodeAudio(blob) {
-        let resolve;
-        const p = new Promise(r => resolve = r);
-        const listener = async () => {
-            const buffer = reader.result;
-            const audio = await audioContext.decodeAudioData(buffer);
-            reader.removeEventListener('loadend', listener);
-            resolve(audio);
-        }
-        reader.addEventListener('loadend', listener);
-        reader.readAsArrayBuffer(blob);
-        return p;
-    }
-
-    recorder.ondataavailable = async (e) => {
-        // const audio = await decodeAudio(e.data);
-        // const samples = audio.getChannelData(0);
-        console.log(`got blob of size ${e.data.size}, starting with...`);
-        console.log(e);
-        const bytes = new Uint8Array(await e.data.slice(128).arrayBuffer());
-        const text = [];
-        for (const byte of bytes) {
-            const char = byte > 31 && byte < 127 ? String.fromCharCode(byte) : '.';
-            text.push(char);
-        }
-        console.log(text.join(''));
-    }
-
-    recorder.start(1000);
-    console.log('started');
-    await new Promise(r => setTimeout(r, 10_000));
-    recorder.stop();
-    console.log('stopped');
-}
-
-// https://web.dev/articles/media-recording-audio
-// https://developer.mozilla.org/en-US/docs/Web/API/AudioWorkletNode/port
-
-async function start2() {
     const audioContext = new AudioContext();
     const stream = await navigator.mediaDevices.getUserMedia({ audio: true });
     const micNode = audioContext.createMediaStreamSource(stream);
--- worklet-decoder.js
+++ worklet-decoder.js
@@ -1,15 +1,83 @@
+// defined by worklet context
+const SAMPLE_RATE = /** @type {number} */ (window.sampleRate);
+
+/** @template T */
+class AsyncQueue {
+    bucket = /** @type {T[]} */ ([]);
+    resolve = /** @type {((item: T) => void) | undefined} */ (undefined);
+
+    /** @param {T} item  */
+    push(item) {
+        if (this.resolve) {
+            this.resolve(item);
+            this.resolve = undefined;
+        } else {
+            this.bucket.push(item);
+        }
+    }
+
+    shift() {
+        const item = this.bucket.shift();
+        if (item) {
+            return Promise.resolve(item);
+        } else {
+            const p = new Promise(r => this.resolve = r);
+            return /** @type {Promise<T>} */ (p);
+        }
+    }
+}
+
 class WorkletDecoder extends AudioWorkletProcessor {
-    first = true;
+    chunks = /** @type {AsyncQueue<Float32Array>} */ (new AsyncQueue());    
+    chunk = new Float32Array();
+    chunkIndex = 0;
+    samples = /** @type {Generator<number>} */ (undefined);
+    prevNegative = 0;
+    waveLength = 0;
+
+    constructor() {
+        super()
+        this.samples = this.getSamples();
+        this.start();
+    }
 
     process(inputs) {
-        if (this.first) {
-            const channels = inputs[0];
-            const samples = channels[0];
-            this.port.postMessage(`channels: ${channels.length}`);
-            this.port.postMessage(`samples: ${samples.length}`);
-            this.first = false;
+        const channels = inputs[0];
+        const chunk = channels[0];
+        if (chunk) {
+            this.chunks.push(chunk);
         }
-        return true;
+
+        // true = safe to clean up this node
+        return !!this.chunk;
+    }
+
+    async * getSamples() {
+        while (true) {
+            if (!this.chunk || this.chunkIndex >= this.chunk.length) {
+                this.chunk = await this.chunks.shift();
+                this.chunkIndex = 0;
+                continue;
+            }
+            yield this.chunk[this.chunkIndex];
+            this.chunkIndex += 1;
+        }
+    }
+
+    async start() {
+        console.log('started');
+        console.log(`sample rate: ${sampleRate}`);
+        for await (const sample of this.samples) {
+            const isNegative = sample < 0;
+            this.waveLength += 1;
+            this.count += 1;
+            if (!this.prevNegative && isNegative) {
+                // falling edge detected
+                console.log(`wavelength ${this.waveLength}`);
+                this.waveLength = 0;
+            }
+            this.prevNegative = isNegative;
+        }
     }
 }
 
